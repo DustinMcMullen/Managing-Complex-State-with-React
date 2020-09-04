bd95d920fa9a15b4e47eaaf45d35d7fec0df249a
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-03 23:10:15 -0500

    Updated to include complex state management

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
+  function declareName(event) {
+    const newName = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "firstName") {
+        return {
+          firstName: newName,
+          lastName: prevValue.lastName
+        };
+      } else if (inputName === "lastName") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: newName
+        };
+      }
+    });
+  }
+  console.log(fullName);
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.firstName} {fullName.lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={declareName}
+          name="firstName"
+          placeholder="First Name"
+          value={fullName.firstName}
+        />
+        <input
+          onChange={declareName}
+          name="lastName"
+          placeholder="Last Name"
+          value={fullName.lastName}
+        />
         <button>Submit</button>
       </form>
     </div>
